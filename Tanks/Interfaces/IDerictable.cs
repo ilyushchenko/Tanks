7750a85a84f0b4908432912eaa6408934bbeda2e
Илющенко Павел <pavel.ks1996@gmail.com>
2016-01-14 12:41:50 +0300

    Добавлен интерфейс направления

---
+++ Tanks/Interfaces/IDerictable.cs
@@ -0,0 +1,12 @@
+﻿namespace Tanks
+{
+    public enum Direction
+    {
+        Up, Down, Left, Right
+    }
+
+    public interface IDirectinable
+    {
+        Direction Direction { get; set; }
+    }
+}
