50d022fb3a0cc1524a648cdf877bf8bccd70e37d
ilyushchenko <pavel.ks1996@gmail.com>
2016-01-21 19:31:15 +0300

    Добавлен интерфейс позиционирования

--- Tanks/Interfaces/IPositionable.cs
+++ Tanks/Interfaces/IPositionable.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace Tanks
 {
     public interface IPositionable
     {
         Point Position { get; set; }
-        //bool Equal(IPositionable unit);
     }
 }
