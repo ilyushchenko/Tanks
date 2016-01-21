b48978bb926bacb91be5dfcd60658916a5f48d6c
ilyushchenko <pavel.ks1996@gmail.com>
2016-01-21 19:40:11 +0300

    Создан контроллер редактора программ

---
+++ Tanks/Controllers/ProgramEditorController.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+
+namespace Tanks
+{
+    public class ProgramEditorController
+    {
+        public void Load(string path)
+        {
+            using(StreamReader sr = new StreamReader(path))
+            {
+                m_title = sr.ReadLine();
+                m_program = "";
+                while(!sr.EndOfStream)
+                {
+                    m_program += sr.ReadLine() + '\n';
+                }
+                m_program = m_program.TrimEnd('\n');
+            }
+        }
+
+        public void Save(string path)
+        {
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(m_title);
+                sw.WriteLine(m_program);
+            }
+        }
+
+        public string GetTitle()
+        {
+            return m_title;
+        }
+
+        public string GetProgram()
+        {
+            return m_program;
+        }
+
+        public void SaveChenges(string title, string program)
+        {
+            m_title = title;
+            m_program = program;
+        }
+
+        private string m_title;
+        private string m_program;
+    }
+}
